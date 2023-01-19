a970db46134d542b54f0d60845c36b048a750b67
abhay <abhaysp9955@gmail.com>
2023-01-19 21:02:02 +0530

    feat: basic setup with daytimetcpcli added

---
+++ introduction/daytimetcpcli.c
@@ -0,0 +1,44 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unp.h>
+
+int
+main(int argc, char **argv)
+{
+	int sockfd = 0, n = 0;
+	char recvline[MAXLINE + 1];
+
+	struct sockaddr_in servaddr;
+
+	if (2 != argc) {
+		err_quit("usage: a.out <IPaddress>");
+	}
+
+	if (0 > (sockfd = socket(AF_INET, SOCK_STREAM, 0))) {
+		err_sys("socket error");
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);  /* daytime server */
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+		err_quit("inet_pton error for %s", argv[1]);
+	}
+
+	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0) {
+		err_sys("connect error");
+	}
+
+	while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+		recvline[n] = 0;  /* null-terminate */
+		if (EOF == fputs(recvline, stdout)) {
+			err_sys("fputs error");
+		}
+	}
+
+	if (0 > n) {
+		err_sys("read error");
+	}
+
+	exit(0);
+}
